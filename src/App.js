68bd33142a84e37ed918d07c0742259ede9416cf
Achmad Wahyu <awahyu253@#gmail.com>
2022-03-25 23:12:03 +0700

    feat: added my pokemon list page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PokemonList from './pages/PokemonList';
+import MyPokemonList from './pages/MyPokemonList';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/my-pokemon">
+          <MyPokemonList/>
+        </Route>
         <Route path="/">
           <PokemonList/>
         </Route>
---
+++ src/pages/MyPokemonList.js
@@ -0,0 +1,71 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { css, jsx } from "@emotion/react";
+import { MdDelete } from 'react-icons/md';
+
+const MyPokemonList = () => {
+  const [pokemonList, setPokemonList] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon/");
+        if (!response) {
+          window.alert("data kosong");
+        } else {
+          setPokemonList(response.data.results);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      My Pokemon List
+      <div
+        css={css`
+          display: flex;
+          flex-flow: column nowrap;
+          gap: 12px;
+          width: 300px;
+          margin: 0 auto;
+        `}
+      >
+        {pokemonList.map((pokemon) => (
+          <div
+            css={css`
+              display: flex;
+              flex-flow: row nowrap;
+              justify-content: space-between;
+              padding: 0 12px;
+              border-radius: 6px;
+              box-shadow: 0px 11px 36px 8px rgba(214,214,214,1);
+              gap: 12px;
+              `}
+          >
+            <h4 
+                css={css`
+                    flex-grow:1
+                `}
+            >{pokemon.name}</h4>
+            <h4>Boba</h4>
+            <div>
+                <MdDelete css={css`
+                    padding-top: 1em;
+                    padding-bottom: 1em;
+                    font-size: 1.2em
+                `}/>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MyPokemonList;
