06a1beae8fbf621bb870a209a5b50f2eafb0c512
Achmad Wahyu <awahyu253@#gmail.com>
2022-03-27 01:07:45 +0700

    feat: utilize localstorage to persist data

--- src/contexts/MyPokemonContext.js
+++ src/contexts/MyPokemonContext.js
@@ -1,10 +1,15 @@
 import { nanoid } from "nanoid";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const MyPokemonContext = React.createContext();
 
 const MyPokemonContextProvider = ({ children }) => {
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState(
+    JSON.parse(localStorage.getItem("pokemon")) || []
+  );
+  useEffect(() => {
+    localStorage.setItem("pokemon", JSON.stringify(pokemon));
+  }, [pokemon]);
 
   const addPokemon = (name, nickName) => {
     setPokemon([...pokemon, { name, nickName, nanoId: nanoid(5) }]);
