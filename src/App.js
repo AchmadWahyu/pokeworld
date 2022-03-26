bed35b0e5e2ee862c36d3657400a7f20a6006a99
Achmad Wahyu <awahyu253@#gmail.com>
2022-03-26 10:14:46 +0700

    feat: added pokemon detail page routes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PokemonList from "./pages/PokemonList";
 import MyPokemonList from "./pages/MyPokemonList";
 import Layout from "./Layout";
+import PokemonDetail from "./pages/PokemonDetail";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Layout>
+          <Route exact path="/pokemon/:pokemon_id">
+            <PokemonDetail />
+          </Route>
           <Route exact path="/my-pokemon">
             <MyPokemonList />
           </Route>
