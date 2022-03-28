1f3fbad8eb1f481c64e076db7d0def05d2ce3ce7
Achmad Wahyu <awahyu253@#gmail.com>
2022-03-28 17:18:21 +0700

    refactor: move catch functionality to detail page

--- src/Layout.js
+++ src/Layout.js
@@ -2,74 +2,19 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/react";
 import {
-  Alert,
   BottomNavigation,
   BottomNavigationAction,
-  Button,
   Container,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
   IconButton,
   Paper,
-  Snackbar,
-  TextField,
 } from "@mui/material";
-import { useContext, useEffect, useState } from "react";
+import { useState } from "react";
 import { MdExplore, MdArrowBack } from "react-icons/md";
 import { SiPocket } from "react-icons/si";
-import { NavLink, useHistory, useLocation, useParams } from "react-router-dom";
-import { MyPokemonContext } from "./contexts/MyPokemonContext";
-import { ReactComponent as PokeBall } from "./icons/pokeball.svg";
+import { NavLink, useHistory, useLocation } from "react-router-dom";
 
 const Layout = ({ children, pageTitle }) => {
-  const { pokemon_name } = useParams();
   const history = useHistory();
-  const { pokemon: myPokemon, addPokemon } = useContext(MyPokemonContext);
-
-  const [openSnackbar, setOpenSnackbar] = useState(false);
-  const [isCaught, setIsCaught] = useState(false);
-  useEffect(() => {
-    if (isCaught) {
-      setOpenAddNickName(true);
-    }
-  }, [isCaught]);
-
-  const [isThrowingPokeball, setIsThrowingPokeball] = useState(false);
-
-  const handleClickCatchPokemon = () => {
-    setIsThrowingPokeball(true);
-    setTimeout(() => {
-      const isPokemonCaught = Math.floor(Math.random() * 2);
-      setIsCaught(isPokemonCaught === 1);
-      setOpenSnackbar(true);
-      setIsThrowingPokeball(false);
-    }, 1000);
-  };
-
-  const [openAddNickName, setOpenAddNickName] = useState(false);
-  const [nickName, setNickName] = useState("");
-  const handleNicknameChange = (e) => {
-    setNickName(e.target.value);
-  };
-
-  const isAlreadyUsed = (name) => {
-    return myPokemon.includes((pokemon) => pokemon.name === name);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (isAlreadyUsed(nickName)) {
-      window.alert("nama sudah digunakan!");
-    } else {
-      addPokemon(pokemon_name, nickName);
-    }
-
-    setIsCaught(false);
-    setOpenAddNickName(false);
-  };
 
   const { pathname } = useLocation();
   const [value, setValue] = useState(pathname);
@@ -144,25 +89,6 @@ const Layout = ({ children, pageTitle }) => {
             component={NavLink}
             to="/"
           />
-          {pokemon_name ? (
-            <BottomNavigationAction
-              label="Throw PokeBall"
-              css={css`
-                &:disabled {
-                  opacity: 0.3;
-                }s
-              `}
-              icon={
-                <PokeBall
-                  css={css`
-                    font-size: 40px;
-                  `}
-                />
-              }
-              disabled={isThrowingPokeball}
-              onClick={handleClickCatchPokemon}
-            />
-          ) : null}
           <BottomNavigationAction
             label="My Pokemon"
             icon={
@@ -178,44 +104,6 @@ const Layout = ({ children, pageTitle }) => {
           />
         </BottomNavigation>
       </Paper>
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={4000}
-        onClose={() => setOpenSnackbar(false)}
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-      >
-        <Alert
-          onClose={() => setOpenSnackbar(false)}
-          severity={isCaught ? "success" : "warning"}
-          sx={{ width: "100%" }}
-        >
-          {isCaught ? "Gotcha!" : "Miss!"}
-        </Alert>
-      </Snackbar>
-      <Dialog keepMounted open={openAddNickName}>
-        <form onSubmit={handleSubmit}>
-          <DialogTitle>Caught Pokemon!</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Add nickname to your new Pokemon!
-            </DialogContentText>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Nickname"
-              type="text"
-              fullWidth
-              variant="standard"
-              onChange={handleNicknameChange}
-              required
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button type="submit">Add Nickname</Button>
-          </DialogActions>
-        </form>
-      </Dialog>
     </Container>
   );
 };
