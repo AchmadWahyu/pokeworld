d15794e78f30785afd28823244840d6313bbe62c
Achmad Wahyu <awahyu253@#gmail.com>
2022-03-28 14:48:53 +0700

    fix: onClick handler params and use mui iconButton

--- src/components/MyPokemonCard.js
+++ src/components/MyPokemonCard.js
@@ -1,6 +1,7 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { css, jsx } from "@emotion/react";
+import { IconButton } from "@mui/material";
 import { useContext } from "react";
 import { MdDelete } from "react-icons/md";
 import { MyPokemonContext } from "../contexts/MyPokemonContext";
@@ -19,6 +20,7 @@ const MyPokemonCard = ({ id, name, nickName }) => {
         display: flex;
         flex-flow: row nowrap;
         justify-content: space-between;
+        align-items: center;
         padding: 0 12px;
         border-radius: 6px;
         box-shadow: 0px 11px 36px 8px rgba(214, 214, 214, 1);
@@ -33,22 +35,13 @@ const MyPokemonCard = ({ id, name, nickName }) => {
         {name}
       </h4>
       <h4>{nickName}</h4>
-      <button
-        type="button"
-        onClick={() => handleClickRemovePokemon()}
-        css={css`
-          background-color: #ffff;
-          border: none;
-        `}
+      <IconButton
+        aria-label="remove-pokemon"
+        size="large"
+        onClick={() => handleClickRemovePokemon(id)}
       >
-        <MdDelete
-          css={css`
-            padding-top: 1em;
-            padding-bottom: 1em;
-            font-size: 1.2em;
-          `}
-        />
-      </button>
+        <MdDelete />
+      </IconButton>
     </div>
   );
 };
